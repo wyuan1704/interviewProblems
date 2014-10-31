2ba8f51e1a51a4f880b854518cb5b1041bb29846
wyuan1704 <wyuan1704@gmail.com>
2014-10-31 18:36:23 +0800

    wyuan; Add a good source http://www-igm.univ-mlv.fr/~lecroq/string/index.html

--- sensitiveWordSearch.cpp
+++ sensitiveWordSearch.cpp
@@ -2,3 +2,6 @@
 // It seems that this is a hard problem. Below are two blogs
 // http://www.iteye.com/topic/336577
 // http://hellohank.iteye.com/blog/1327903
+// Stackoverflow is better
+// http://stackoverflow.com/questions/3183582/what-is-the-fastest-substring-search-algorithm
+// The best souce is http://www-igm.univ-mlv.fr/~lecroq/string/index.html
